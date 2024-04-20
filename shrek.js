8543cedd154ef5c721784360d6ae9f22d9184604
Luciano Moyano <luciano.moyano@liesa.com.ar>
2024-04-20 17:02:52 -0300

    [UPD] - cambios en la logica

--- shrek.js
+++ shrek.js
@@ -13,43 +13,129 @@ el total de puntos de recompensa que obtendría Shrek si cumple las misiones, es
 
 */
 
+//Lo primero que hago es definir las misiones y las caracteristicas correspondientes
 const misiones = [
-    { nombre: "Liberar una princesa", solicitante: "Lord Farquaad", trolls: 4 },
-    { nombre: "Buscar un objeto mágico", solicitante: "Señor Gandalf", distancia: 40 }
+  {
+    nombre: "Liberar una princesa",
+    solicitante: "Lord Farquaad",
+    detalles: { trolls: 4 },
+  },
+  {
+    nombre: "Buscar un objeto mágico",
+    solicitante: "Señor Gandalf",
+    detalles: { distancia: 40 },
+  },
 ];
 
-function esMisionDificil(mision) {
-    if (mision.nombre.startsWith('L') && (mision.trolls === 4 || mision.trolls === 5)) {
-        return true;
-    } else if (mision.nombre.startsWith('B') && mision.distancia > 100) {
-        return true;
-    }
-    return false;
+//Necesito definir la clase "Misión" con el constructor y los parametros del mismo.
+
+class Mision {
+  constructor(nombre, solicitante, detalle) {
+    (this.nombre = nombre),
+      (this.solicitante = solicitante),
+      (this.detalle = detalle);
+  }
+
+  esDificil() {
+    return this.solicitante.charAt(0) === "G";
+  }
+
+  puntos() {
+    return 0;
+  }
 }
 
-function procesarMisiones(misiones) {
-    let misionesDificiles = [];
-    let solicitantes = [];
-    let totalPuntosRecompensa = 0;
+class MisionPrincesa extends Mision {
+  constructor(solicitante, trolls) {
+    super("Liberar una princesa", solicitante, trolls);
+  }
 
-    for (let i = 0; i < misiones.length; i++) {
-        let mision = misiones[i];
-        if (esMisionDificil(mision)) {
-            misionesDificiles.push(mision.nombre);
-            solicitantes.push(mision.solicitante);
-            if (mision.nombre.startsWith('L')) {
-                totalPuntosRecompensa += mision.trolls * 2;
-            } else {
-                totalPuntosRecompensa += (mision.distancia === 50) ? 10 : 5;
-            }
-        }
-    }
+  esDificil() {
+    return super.esDificil() && (this.detalle === 4 || this.detalle === 5);
+  }
 
-    return { misionesDificiles, solicitantes, totalPuntosRecompensa };
+  puntos() {
+    return this.detalle * 2;
+  }
 }
 
-const resultados = procesarMisiones(misiones);
-console.log("Misiones difíciles:", resultados.misionesDificiles);
-console.log("Solicitantes:", resultados.solicitantes);
-console.log("Total de puntos de recompensa:", resultados.totalPuntosRecompensa);
+class MisionObjeto extends Mision {
+  constructor(solicitante, distancia) {
+    super("Buscar un objeto magico", solicitante, distancia);
+  }
 
+  esDificil() {
+    return super.esDificil() && this.detalle > 100;
+  }
+
+  puntos() {
+    return this.detlle <= 50 ? 10 : 5;
+  }
+}
+
+class Shrek {
+  constructor() {
+    this.misiones = [];
+  }
+  agregarMision(mision) {
+    this.misiones.push(mision);
+  }
+
+  misionesDificiles() {
+    return this.misiones.filter((m) => m.esDificil());
+  }
+
+  // Lo que hace reduce es iterar sobre todas las misiones que existen sumando los puntos de cada mision a un total acumulado. Si shrek completa todas las misiones esos serian los puntos
+  totalPuntosRecompensa() {
+    return this.misionesDificiles().reduce(
+      (total, mision) => total + mision.puntos(),
+      0
+    );
+  }
+}
+
+const shrek = new Shrek();
+
+shrek.agregarMision(new MisionPrincesa("Lord Farquaad", 4));
+shrek.agregarMision(new MisionObjeto("Señor Gandalf", 40));
+
+console.log("Mostrar misiones dificiles: ", shrek.misionesDificiles());
+console.log(
+  "Mostrar total de puntos de recompensas: ",
+  shrek.totalPuntosRecompensa()
+);
+
+// function esMisionDificil(mision) {
+//     if (mision.nombre.startsWith('L') && (mision.trolls === 4 || mision.trolls === 5)) {
+//         return true;
+//     } else if (mision.nombre.startsWith('B') && mision.distancia > 100) {
+//         return true;
+//     }
+//     return false;
+// }
+
+// function procesarMisiones(misiones) {
+//     let misionesDificiles = [];
+//     let solicitantes = [];
+//     let totalPuntosRecompensa = 0;
+
+//     for (let i = 0; i < misiones.length; i++) {
+//         let mision = misiones[i];
+//         if (esMisionDificil(mision)) {
+//             misionesDificiles.push(mision.nombre);
+//             solicitantes.push(mision.solicitante);
+//             if (mision.nombre.startsWith('L')) {
+//                 totalPuntosRecompensa += mision.trolls * 2;
+//             } else {
+//                 totalPuntosRecompensa += (mision.distancia === 50) ? 10 : 5;
+//             }
+//         }
+//     }
+
+//     return { misionesDificiles, solicitantes, totalPuntosRecompensa };
+// }
+
+// const resultados = procesarMisiones(misiones);
+// console.log("Misiones difíciles:", resultados.misionesDificiles);
+// console.log("Solicitantes:", resultados.solicitantes);
+// console.log("Total de puntos de recompensa:", resultados.totalPuntosRecompensa);
